9440f1f84c9b6fbc59e15e523364fcf7affb9651
1398565099@qq.com <wmm911018!>
2018-09-18 11:43:48 +0800

    柱状图分组

--- app/src/main/java/com/android/ming/mpandroidchart/java/BarChartActivity.java
+++ app/src/main/java/com/android/ming/mpandroidchart/java/BarChartActivity.java
@@ -1,15 +1,16 @@
 package com.android.ming.mpandroidchart.java;
 
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -18,11 +19,13 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BarChartActivity extends AppCompatActivity {
 
     private BarChart mChat;
-    private String[] mValues = new String[]{"一月", "二月", "三月", "四月", "五月", "六月", "七月"};
+    private String[] mValues = new String[]{"一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"};
+    private List<String> mGroup = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,26 +37,42 @@ public class BarChartActivity extends AppCompatActivity {
     }
 
     private void initGroup() {
-        Float[] group1 = new Float[]{12f, 18f, 9f, 23f, 25f, 41f};
-        Float[] group2 = new Float[]{10f, 19f, 22f, 15f, 32f, 39f};
+//        Float[] group1 = new Float[]{12f, 18f, 9f, 23f, 25f, 41f};
+//        Float[] group2 = new Float[]{10f, 19f, 22f, 15f, 32f, 39f};
         List<BarEntry> entriesGroup1 = new ArrayList<>();
         List<BarEntry> entriesGroup2 = new ArrayList<>();
-        for (int i = 0; i < group1.length; i++) {
-            entriesGroup1.add(new BarEntry(i, group1[i]));
-            entriesGroup2.add(new BarEntry(i, group2[i]));
+        List<BarEntry> entriesGroup3 = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            entriesGroup1.add(new BarEntry(i, new Random().nextInt(20) + 20));
+            entriesGroup2.add(new BarEntry(i, new Random().nextInt(20) + 20));
+            entriesGroup3.add(new BarEntry(i, new Random().nextInt(20) + 20));
+            mGroup.add("第" + (i + 1) + "季度");
         }
         BarDataSet set1 = new BarDataSet(entriesGroup1, "group1");
         set1.setColor(Color.RED);
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
         BarDataSet set2 = new BarDataSet(entriesGroup2, "group2");
         set2.setColor(Color.GREEN);
-        BarData data = new BarData(set1,set2);
-        data.setBarWidth(0.45f);
+        set2.setAxisDependency(YAxis.AxisDependency.LEFT);
+        BarDataSet set3 = new BarDataSet(entriesGroup3, "group3");
+        set3.setColor(Color.BLUE);
+        set3.setAxisDependency(YAxis.AxisDependency.LEFT);
+        BarData data = new BarData(set1, set2, set3);
+        data.setBarWidth(0.2f);
         mChat.setData(data);
-        mChat.groupBars(1980f, 0.06f, 0.02f);
+        mChat.groupBars(-0.6f, 0.3f, 0.05f);
         mChat.invalidate();
         Description description = new Description();
         description.setText("公司前半年财务报表(单位：万元)");
         mChat.setDescription(description);
+        XAxis xAxis = mChat.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mGroup.get((int) value);
+            }
+        });
     }
 
     private void intiView() {
@@ -81,7 +100,8 @@ public class BarChartActivity extends AppCompatActivity {
         xAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                return null;
+                Log.i("110", axis.toString());
+                return mValues[(int) value];
             }
         });
     }
